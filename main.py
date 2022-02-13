703bd67554411efc84590729eca42dc454f0edad
Max <maximatie@gmail.com>
2022-02-13 20:02:39 +0300

    v0.00000000003

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-operation = int(input('1.Сложение \n2.Вычитание \n3.Умножение \n4.Деление \nВведите номер операции: '))
-fn = int(input('Введите первое число: '))
-sn = int(input('Введите второе число: '))
-if operation == 1:
-    print(fn, '+', sn, '=', fn+sn)
-elif operation == 2:
-    print(fn, '-', sn, '=', fn-sn)
-elif operation == 3:
-    print((fn, '*', sn, '=', fn*sn))
-elif operation == 4:
-    print((fn, '/', sn, '=', fn//sn))
+lang = int(input('1.Russian \n2.English \n'))
+if lang == 2:
+    operation = int(input('1.Addition \n2.Subtraction \n3.Multiplication \n4.Division '
+                          '\nEnter operation number: '))
+    fn = int(input('Enter first number: '))
+    sn = int(input('Enter second number: '))
+    if operation == 1:
+        print(fn, '+', sn, '=', fn + sn)
+    elif operation == 2:
+        print(fn, '-', sn, '=', fn - sn)
+    elif operation == 3:
+        print((fn, '*', sn, '=', fn * sn))
+    elif operation == 4:
+        print((fn, '/', sn, '=', fn // sn))
+    else:
+        print('Error')
 else:
-    print('Неверная операция')
+    operation = int(input('1.Сложение \n2.Вычитание \n3.Умножение \n4.Деление \nВведите номер операции: '))
+    fn = int(input('Введите первое число: '))
+    sn = int(input('Введите второе число: '))
+    if operation == 1:
+        print(fn, '+', sn, '=', fn + sn)
+    elif operation == 2:
+        print(fn, '-', sn, '=', fn - sn)
+    elif operation == 3:
+        print(fn, '*', sn, '=', fn * sn)
+    elif operation == 4:
+        print(fn, '/', sn, '=', fn // sn)
+    else:
+        print('Неверная операция')
